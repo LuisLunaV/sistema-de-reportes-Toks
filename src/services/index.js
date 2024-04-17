ed972e1713a3c378521e9f069d10b9d8c992d971
luislunav <luis.a.luna.v@outlook.com>
2024-04-16 20:49:53 -0600

    modificamos la altura del la barra nav

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 const api = {
-    url: 'http://localhost:8080',
+    // url: 'http://localhost:8080',
+    url: 'http://192.168.100.11:8080',
     auth: '/api/v1/auth',
     createUser: '/api/v1/users',
 };
